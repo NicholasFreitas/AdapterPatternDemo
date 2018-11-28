eb30d5643c988991400eb030f3bce9f02528c523
Nicholas Freitas <nicholasfreitas@sympatico.ca>
2018-11-28 17:00:43 -0500

    Adapter Demo
    the purpose of the demo is to demonstrate the adapter pattern. Which allows two different contracts to interface with one another.

---
+++ ConcreteTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDemo
+{
+    class ConcreteTarget : ITarget
+    {
+        public void Request()
+        {
+            Console.WriteLine("Specific Implementation");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDemo
+{
+    class Program
+    {
+        /*
+         Adapter Pattern : the main purpose of the adapter pattern is to adapt one interface to another.
+
+            ---------       ------------
+            |Client | ----> |Target    |
+            ---------       ------------
+                            |Request() |
+                            ------------
+                                 |
+                                 ^
+                                 ^
+                                 |
+                             -----------        --------------------
+                             |Adapter  | --->>--|Adaptee           |
+                             -----------        --------------------
+       SpecificRequest()---o |Request()|        |SpecificRequest() |
+                             -----------        --------------------
+        */
+        
+        static void Main(string[] args)
+        {
+
+            var target = new ConcreteTarget();
+            var adaptee = new ConcreteAdaptee();
+            var adapter = new ConcreteAdapter(adaptee);
+
+            adapter.Request();
+                                                                        
+        }
+    }
+}
